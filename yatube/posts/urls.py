14c1cc81d6b2cc5a3e89cafb1a14acb371c1a506
Нурбек Орозалиев <nurik878878@yandex.ru>
2022-12-04 18:12:50 +0500

    Добавлены CSS и img

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,9 +1,11 @@
 # posts/urls.py
 from django.urls import path
-
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index),
-    path('group/<slug:slug>/', views.group_posts),
+    path('', views.index, name='main_page'),
+    path('group_posts/', views.group_posts, name='group_posts'),
+    path('group_list.html/', views.group_list, name='group_list'),
 ] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,9 +3,23 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Yatube - социальная сеть')
+    template = 'posts/index.html'
+    context = {
+        'text': 'Это главная страница проекта Yatube.',
+    }   
+    return render(request, template, context) 
 
 
 # В урл мы ждем парметр, и нужно его прередать в функцию для использования
-def group_posts(request, slug):
-    return HttpResponse(f'Посты группы {slug}')
+def group_posts(request):
+    template = 'posts/group_posts.html'
+    # Словарь с данными принято называть context
+    context = {
+        'text': 'Здесь будет информация о группах проекта Yatube',
+    }   
+    return render(request, template, context) 
+
+# В урл мы ждем парметр, и нужно его прередать в функцию для использования
+def group_list(request):
+    template = 'posts/group_list.html'
+    return render(request, template) 
